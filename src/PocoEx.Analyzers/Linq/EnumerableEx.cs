a9643d7ad09995f6dc23760e2e4595f12ef90d53
奥野 大介 <dai_okuno@hotmail.com>
2016-03-20 15:25:39 +0900

    コミット漏れ。

---
+++ src/PocoEx.Analyzers/Linq/EnumerableEx.cs
@@ -0,0 +1,28 @@
+﻿using PocoEx.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocoEx.Linq
+{
+    public static class EnumerableEx
+    {
+        public static IEnumerable<T> Remove<T>(this IEnumerable<T> source, T item)
+            => Remove(source, item, EqualityComparer<T>.Default);
+
+        public static IEnumerable<T> Remove<T>(this IEnumerable<T> source, T item, IEqualityComparer<T> comparer)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (comparer == null) throw new ArgumentNullException(nameof(comparer));
+
+            return new RemoveEnumerable<T>(source)
+            {
+                Removing = item,
+                Comparer = comparer,
+            };
+        }
+
+    }
+}
